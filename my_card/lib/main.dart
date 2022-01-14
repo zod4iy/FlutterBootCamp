95e5fdea7083cf1808cd539e0a526f2db88d1037
a.kurylenko <>
2022-01-14 16:26:04 +0200

    [my_card] - Add phone/email fields

--- my_card/lib/main.dart
+++ my_card/lib/main.dart
@@ -14,27 +14,73 @@ class MyApp extends StatelessWidget {
           child: Column(
             children: [
               const CircleAvatar(
-                radius: 50.0,
-                backgroundColor: Colors.white,
-                foregroundImage: AssetImage('images/userMale.png')
-              ),
+                  radius: 50.0,
+                  backgroundColor: Colors.white,
+                  foregroundImage: AssetImage('images/userMale.png')),
               const Text(
                 'Alexandr Kurylenko',
                 style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 34.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white
-                ),
+                    fontFamily: 'Pacifico',
+                    fontSize: 34.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
               ),
               Text(
                 'iOS/Flutter DEVELOPER',
                 style: TextStyle(
-                    fontFamily: 'SourceSansPro',
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.teal.shade100,
-                    letterSpacing: 2.5,
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      '097-123-45-67',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                        fontFamily: 'SourceSansPro',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      'example@google.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                        fontFamily: 'SourceSansPro',
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
