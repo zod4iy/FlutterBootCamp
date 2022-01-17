2e7add62445b03921fbb274a7564c56cccf32c7c
a.kurylenko <>
2022-01-17 19:19:29 +0200

    [quizzler] - Refactor with QiuzBrain

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quizzler/question.dart';
+import 'package:quizzler/quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(const Quizzler());
 
@@ -32,22 +34,13 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<Question> questions = [
-    Question('You can lead a cow down stairs but not up stairs.', false),
-    Question('Approximately one quarter of human bones are in the feet.', true),
-    Question('A slug\'s blood is green.', true)
-  ];
-
-  int questionNumber = 0;
-
   void checkQuestionFor({required bool answer}) {
-    if (answer == questions[questionNumber].questionAnswer) {
-      scoreKeeper.add(Icon(Icons.check, color: Colors.green));
-    } else {
-      scoreKeeper.add(Icon(Icons.close, color: Colors.red));
-    }
     setState(() {
-      questionNumber++;
+      answer == quizBrain.getQuestionAnswer()
+        ? scoreKeeper.add(Icon(Icons.check, color: Colors.green))
+        : scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+
+      quizBrain.nextQuestion();
     });
   }
 
@@ -63,7 +56,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber].questionText,
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -118,9 +111,3 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
