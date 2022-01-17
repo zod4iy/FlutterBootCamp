5741d99d40214b865ed4f043485670480a1073b9
a.kurylenko <>
2022-01-17 17:21:09 +0200

    [quizzler] - Create Question class

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
 
 void main() => runApp(const Quizzler());
 
@@ -30,19 +31,17 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
 
-  List<bool> answers = [false, true, true];
+  List<Question> questions = [
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true)
+  ];
 
   int questionNumber = 0;
 
   void checkQuestionFor({required bool answer}) {
-    bool correctAnswer = answers[questionNumber];
-    if (answer == correctAnswer) {
+    if (answer == questions[questionNumber].questionAnswer) {
       scoreKeeper.add(Icon(Icons.check, color: Colors.green));
     } else {
       scoreKeeper.add(Icon(Icons.close, color: Colors.red));
@@ -64,7 +63,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questions[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
