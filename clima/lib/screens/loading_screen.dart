c6459e812fdba75defc3fc48947d885e91f20f63
a.kurylenko <>
2022-01-21 16:55:08 +0200

    [clima] - Perform request

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,9 +1,7 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:clima/services/networking.dart';
 import 'dart:convert';
-
-const String apiKey = '74f903ea70161f3903e0a7dae7464d1e';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -17,38 +15,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
 
-    _getLocation();
+    _getLocationData();
   }
 
-  void _getLocation() async {
-    await location.getCurrentLocation();
+  void _getLocationData() async {
+    ApiWeatherService weatherService = ApiWeatherService();
 
-    _getData(
-        long: location.longitude ?? 0.0,
-        lat: location.latitude ?? 0.0,
-    );
-  }
+    var weatherData = await weatherService.fetchWeatherData();
 
-  void _getData({required double long, required double lat}) async {
-    http.Response response = await http.get(
-        Uri.parse(
-            'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$long&appid=$apiKey'
-        ),
-    );
-
-    if (response.statusCode == 200) {
-      String data = response.body;
-
-      var decodeData = jsonDecode(data);
-
-      double temperature = decodeData['main']['temp'];
-      int condition = decodeData['weather'][0]['id'];
-      String cityName = decodeData['name'];
-
-      print(cityName);
-    } else {
-      print(response.statusCode);
-    }
+    var cityName = weatherData['name'];
+    print(cityName);
   }
 
   @override
---
+++ clima/lib/services/networking.dart
@@ -0,0 +1,43 @@
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const String kApiKey = '74f903ea70161f3903e0a7dae7464d1e';
+
+class BasicApiService {
+  String? url;
+
+  BasicApiService({this.url});
+
+  Future performGetRequest() async {
+    http.Response response = await http.get(
+      Uri.parse(url ?? ''),
+    );
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
+
+class ApiWeatherService extends BasicApiService {
+  ApiWeatherService() : super();
+
+  Future fetchWeatherData() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    try {
+      String long = location.longitude.toString();
+      String lat = location.latitude.toString();
+
+      url = 'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$long&appid=$kApiKey';
+      return performGetRequest();
+    } catch (e){
+      print(e);
+    }
+  }
+}
