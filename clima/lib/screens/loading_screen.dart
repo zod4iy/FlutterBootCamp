55340e2164e61f21b8a6690be289fa4393f20eaf
a.kurylenko <>
2022-01-21 14:53:02 +0200

    [clima] - Test to fetch weather data

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,6 +1,9 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const String apiKey = '74f903ea70161f3903e0a7dae7464d1e';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -19,8 +22,33 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void _getLocation() async {
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+
+    _getData(
+        long: location.longitude ?? 0.0,
+        lat: location.latitude ?? 0.0,
+    );
+  }
+
+  void _getData({required double long, required double lat}) async {
+    http.Response response = await http.get(
+        Uri.parse(
+            'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$long&appid=$apiKey'
+        ),
+    );
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      var decodeData = jsonDecode(data);
+
+      double temperature = decodeData['main']['temp'];
+      int condition = decodeData['weather'][0]['id'];
+      String cityName = decodeData['name'];
+
+      print(cityName);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
