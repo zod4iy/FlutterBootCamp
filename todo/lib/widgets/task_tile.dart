af8d9d012f57bae59e2c2072b21187944aee7b9b
a.kurylenko <>
2022-01-26 13:06:28 +0200

    [todo] - Refactor TaskTile with Callbacks

--- todo/lib/widgets/task_tile.dart
+++ todo/lib/widgets/task_tile.dart
@@ -1,34 +1,57 @@
 import 'package:flutter/material.dart';
 
-class Todo {
-  final String title;
-  bool isDone = false;
-
-  Todo({
-    required this.title,
-  });
-}
-
-class TaskTile extends StatelessWidget {
-  final Todo model;
+class TaskTile extends StatefulWidget {
 
   const TaskTile({
     Key? key,
-    required this.model,
-
   }) : super(key: key);
+
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return Container(
         padding: EdgeInsets.symmetric(vertical: 8.0),
         child: ListTile(
-          title: Text(model.title),
-          trailing: Checkbox(
-            value: model.isDone,
-            onChanged: (bool? value) {  },
+          title: Text(
+            'Task number ?',
+            style: TextStyle(
+               decoration: isChecked ? TextDecoration.lineThrough : null,
+            )
+          ),
+          trailing: TaskCheckbox(
+            checkboxState: isChecked,
+            toggleCheckboxState: (bool? checkboxState) {
+              setState(() {
+                isChecked = checkboxState ?? false;
+              });
+            },
           ),
         )
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final void Function(bool?) toggleCheckboxState;
+
+  TaskCheckbox({
+    required this.checkboxState,
+    required this.toggleCheckboxState,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
